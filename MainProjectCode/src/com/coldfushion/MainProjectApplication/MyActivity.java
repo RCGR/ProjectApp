1cf39a5f4aa2c423c2cc9b8a517ee9bbdf6e595e
Ceesie12345 <ceesjannolen@live.nl>
2015-05-19 20:20:27 +0200

    added first applook with maps

---
+++ MainProjectCode/src/com/coldfushion/MainProjectApplication/MyActivity.java
@@ -0,0 +1,42 @@
+package com.coldfushion.MainProjectApplication;
+
+
+
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MyActivity extends Activity implements OnMapReadyCallback {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        MapFragment mapFragment = (MapFragment) getFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+    }
+    @Override
+    public void onMapReady(GoogleMap map){
+        LatLng sydney = new LatLng(-33.867, 151.206);
+
+        map.setMyLocationEnabled(true);
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 13));
+
+        map.addMarker(new MarkerOptions()
+                .title("Sydney")
+                .snippet("The most populous city in Australia.")
+                .position(sydney));
+    }
+}
