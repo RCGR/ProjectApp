87096313c3896a1deb2a62278a2e25ac0e0d27c8
RobertKraaijeveld <0890289@hr.nl>
2015-05-21 11:56:31 +0200

    Robert: Created phpdatabase scripts

---
+++ DatabaseScripts/php scripts/db_config.php
@@ -0,0 +1,12 @@
+<?php
+ 
+/*
+ * Dit zijn alle variabelen die bij het connecten met de DB gebruikt worden, in key-valuepairs
+ */
+ 
+define('UITJES_DB_USER', "sql577884"); // db user
+define('UITJES_DB_PASSWORD', "hC6*bN8!"); // db password 
+define('UITJES_DB_DATABASE', "sql577884"); // database name
+define('UITJES_DB_SERVER', "sql5.freemysqlhosting.net"); // db server
+
+?>
