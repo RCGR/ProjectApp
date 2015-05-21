5529d1a1dbafc90ea52391d65dca47adc6d8ad39
Ceesie12345 <ceesjannolen@live.nl>
2015-05-21 11:44:36 +0200

    removed places api

--- MainProjectCode/src/com/coldfushion/MainProjectApplication/MyActivity.java
+++ MainProjectCode/src/com/coldfushion/MainProjectApplication/MyActivity.java
@@ -1,27 +1,9 @@
 package com.coldfushion.MainProjectApplication;
 
-
-
-
 import android.app.Activity;
-import android.app.Dialog;
-import android.app.DialogFragment;
-import android.content.Context;
-import android.content.DialogInterface;
-import android.content.Intent;
-import android.content.IntentSender;
+
 import android.os.Bundle;
 
-import android.os.Looper;
-import android.util.Log;
-import android.view.View;
-import android.widget.Toast;
-import com.google.android.gms.common.*;
-import com.google.android.gms.common.api.*;
-import com.google.android.gms.location.places.Place;
-import com.google.android.gms.location.places.PlaceBuffer;
-import com.google.android.gms.location.places.Places;
-import com.google.android.gms.location.places.ui.PlacePicker;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
@@ -30,18 +12,8 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 
-public class MyActivity extends Activity implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener
-{
+public class MyActivity extends Activity implements OnMapReadyCallback{
 
-    private GoogleApiClient mGoogleApiClient;
-int PLACE_PICKER_REQUEST;
-    // Request code to use when launching the resolution activity
-    private static final int REQUEST_RESOLVE_ERROR = 1001;
-    // Unique tag for the error dialog fragment
-    private static final String DIALOG_ERROR = "dialog_error";
-    // Bool to track whether the app is already resolving an error
-    private boolean mResolvingError = false;
-    String placeId;
     GoogleMap Theonemap;
     /**
      * Called when the activity is first created.
@@ -55,15 +27,7 @@ int PLACE_PICKER_REQUEST;
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
-        mGoogleApiClient = new GoogleApiClient
-                .Builder(this)
-                .addApi(Places.GEO_DATA_API)
-                .addApi(Places.PLACE_DETECTION_API)
-                .addConnectionCallbacks(this)
-                .addOnConnectionFailedListener(this)
-                .build();
 
-        mResolvingError = savedInstanceState != null && savedInstanceState.getBoolean(STATE_RESOLVING_ERROR, false);
     }
 
     @Override
@@ -79,163 +43,4 @@ int PLACE_PICKER_REQUEST;
                 .position(sydney));
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        mGoogleApiClient.connect();
-    }
-
-    @Override
-    protected void onStop() {
-        mGoogleApiClient.disconnect();
-        super.onStop();
-    }
-
-
-
-
-
-
-
-        @Override
-        protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-
-                if (requestCode == PLACE_PICKER_REQUEST) {
-                    if (resultCode == RESULT_OK) {
-                        // Make sure the app is not already connected or attempting to connect
-                        if (!mGoogleApiClient.isConnecting() &&
-                                !mGoogleApiClient.isConnected()) {
-                            mGoogleApiClient.connect();
-                        }
-                        Place place = PlacePicker.getPlace(data, this);
-                        placeId = place.getId();
-                        String toastMsg = String.format("Place: %s", place.getName());
-                        Toast.makeText(this, toastMsg, Toast.LENGTH_LONG).show();
-                        Theonemap.moveCamera(CameraUpdateFactory.newLatLngZoom(place.getLatLng(), 13));
-                        Theonemap.addMarker(new MarkerOptions().position(place.getLatLng()).title(place.getName().toString()).snippet(place.getAddress().toString()) );
-                    }
-
-            }
-        }
-
-
-
-    private static final String STATE_RESOLVING_ERROR = "resolving_error";
-    @Override
-    protected void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-        outState.putBoolean(STATE_RESOLVING_ERROR, mResolvingError);
-    }
-
-
-
-
-    //functions for the apiclient
-    @Override
-    public void onConnectionSuspended(int cause) {
-
-        // The connection has been interrupted.
-        // Disable any UI components that depend on Google APIs
-        // until onConnected() is called.
-    }
-    @Override
-    public void onConnected(Bundle connectionHint) {
-        // Connected to Google Play services!
-        // The good stuff goes here.
-    }
-    @Override
-    public void onConnectionFailed(ConnectionResult result) {
-        if (mResolvingError) {
-            // Already attempting to resolve an error.
-            return;
-        } else if (result.hasResolution()) {
-            try {
-                mResolvingError = true;
-                result.startResolutionForResult(this, REQUEST_RESOLVE_ERROR);
-            } catch (IntentSender.SendIntentException e) {
-                // There was an error with the resolution intent. Try again.
-                mGoogleApiClient.connect();
-            }
-        } else {
-            // Show dialog using GooglePlayServicesUtil.getErrorDialog()
-            showErrorDialog(result.getErrorCode());
-            mResolvingError = true;
-        }
-    }
-
-
-
-    public void test1234(View v){
-        PLACE_PICKER_REQUEST = 1;
-
-        try {
-            PlacePicker.IntentBuilder intentBuilder =
-                    new PlacePicker.IntentBuilder();
-            Intent intent = intentBuilder.build(this);
-            // Start the intent by requesting a result,
-            // identified by a request code.
-            startActivityForResult(intent, PLACE_PICKER_REQUEST);
-
-
-
-
-        } catch (GooglePlayServicesRepairableException e) {
-            // ...
-        } catch (GooglePlayServicesNotAvailableException e) {
-            // ...
-        }
-
-//        while(placeId == null){
-//            Toast.makeText(getApplicationContext(), "wachten", Toast.LENGTH_SHORT).show();
-//        }
-//        Places.GeoDataApi.getPlaceById(mGoogleApiClient, placeId).setResultCallback(new ResultCallback<PlaceBuffer>() {
-//            @Override
-//            public void onResult(PlaceBuffer places) {
-//                if (places.getStatus().isSuccess()) {
-//                    final Place myPlace = places.get(0);
-//                    Toast.makeText(getApplicationContext(), myPlace.getId() + " is het id, dit is de naam: " + myPlace.getName()  , Toast.LENGTH_LONG).show();
-//                    Theonemap.moveCamera(CameraUpdateFactory.newLatLngZoom(myPlace.getLatLng(), 5));
-//                    Theonemap.addMarker(new MarkerOptions().title(myPlace.getName().toString()).snippet(myPlace.getAddress().toString()).position(myPlace.getLatLng()));
-//
-//                }
-//                places.release();
-//            }
-//        });
-    }
-
-
-
-    //special class for the error dialog
-    public static class ErrorDialogFragment extends DialogFragment {
-        public ErrorDialogFragment() {
-        }
-
-        @Override
-        public Dialog onCreateDialog(Bundle savedInstanceState) {
-            // Get the error code and retrieve the appropriate dialog
-            int errorCode = this.getArguments().getInt(DIALOG_ERROR);
-            return GooglePlayServicesUtil.getErrorDialog(errorCode,
-                    this.getActivity(), REQUEST_RESOLVE_ERROR);
-        }
-
-        @Override
-        public void onDismiss(DialogInterface dialog) {
-            ((MyActivity) getActivity()).onDialogDismissed();
-        }
-    }
-
-    private void showErrorDialog(int errorCode) {
-        // Create a fragment for the error dialog
-        ErrorDialogFragment dialogFragment = new ErrorDialogFragment();
-        // Pass the error that should be displayed
-        Bundle args = new Bundle();
-        args.putInt(DIALOG_ERROR, errorCode);
-        dialogFragment.setArguments(args);
-        dialogFragment.show(getFragmentManager(), "errordialog");
-    }
-
-    public void onDialogDismissed() {
-        mResolvingError = false;
-    }
 }
-
